34fb9c3f99ed3b3814523bde93d959d90a6bcf34
charanvinay <charanvinaynarni@gmail.com>
2021-11-11 12:42:26 +0530

    users

--- lib/Api/firebase_api.dart
+++ lib/Api/firebase_api.dart
@@ -1,9 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:twitterp/Models/tweet_model.dart';
 
 class FirebaseApi {
   static Future<String> createTweet(Tweet tweet) async {
-    final docTweet = FirebaseFirestore.instance.collection('tweet').doc();
+    final user = FirebaseAuth.instance.currentUser;
+    final docTweet = FirebaseFirestore.instance.collection(user!.uid).doc();
 
     tweet.id = docTweet.id;
     await docTweet.set(tweet.toJson());
@@ -12,22 +14,25 @@ class FirebaseApi {
   }
 
   static Stream<QuerySnapshot> readTweets() {
+    final user = FirebaseAuth.instance.currentUser;
     return FirebaseFirestore.instance
-        .collection('tweet')
+        .collection(user!.uid)
         .orderBy(TweetField.createdTime, descending: true)
         .snapshots();
   }
 
   static Future updateTweet(Tweet tweet) async {
+    final user = FirebaseAuth.instance.currentUser;
     final docTweet =
-        FirebaseFirestore.instance.collection('tweet').doc(tweet.id);
+        FirebaseFirestore.instance.collection(user!.uid).doc(tweet.id);
 
     await docTweet.update(tweet.toJson());
   }
 
   static Future deleteTweet(Tweet tweet) async {
+    final user = FirebaseAuth.instance.currentUser;
     final docTweet =
-        FirebaseFirestore.instance.collection('tweet').doc(tweet.id);
+        FirebaseFirestore.instance.collection(user!.uid).doc(tweet.id);
 
     await docTweet.delete();
   }
